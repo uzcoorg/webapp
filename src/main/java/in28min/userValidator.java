caa990ee5121e21e1bb80ccb7ee7e4749b21cc0d
allen <uzcoorg@gmail.com>
2017-12-10 12:31:43 -0500

    comit 17.1

--- src/main/java/in28min/userValidator.java
+++
@@ -1,7 +0,0 @@
-package in28min;
-
-public class userValidator {
-    public boolean validateUser(String user, String password) {
-        return user.trim().equalsIgnoreCase("allen") && password.equals("dummy");
-    }
-}
